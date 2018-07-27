f2ab77957b916ac3704dd9969d0a3b7cb2a5ad39
Archit Sharma <archits1996@gmail.com>
2018-07-27 19:46:07 +0530

    Change in take camera image

--- app/src/main/java/com/archit/ocr/MainActivity.java
+++ app/src/main/java/com/archit/ocr/MainActivity.java
@@ -21,8 +21,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Pair;
@@ -45,14 +47,18 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import static android.media.MediaRecorder.VideoSource.CAMERA;
 
 public class MainActivity extends AppCompatActivity  {
     private static final String TAG = "MainActivity";
+
     private ImageView mImageView;
     private Button mButton;
     private Button mCloudButton;
@@ -60,16 +66,22 @@ public class MainActivity extends AppCompatActivity  {
     private Bitmap mSelectedImage;
     private Bitmap mSelectedImage1;
     private GraphicOverlay mGraphicOverlay;
+
     // Max width (portrait mode)
     private Integer mImageMaxWidth;
     // Max height (portrait mode)
     private Integer mImageMaxHeight;
     public Integer GALLERY;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    String mCurrentPhotoPath;
+    public Integer REQUEST_TAKE_PHOTO;
+    private Uri camera_uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         GALLERY=1000;
+        REQUEST_TAKE_PHOTO=2000;
         mImageView = findViewById(R.id.image_view);
 
         mButton = findViewById(R.id.button_text);
@@ -79,7 +91,9 @@ public class MainActivity extends AppCompatActivity  {
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                runTextRecognition();
+                if(mSelectedImage != null) {
+                    runTextRecognition();
+                }
             }
         });
         mCloudButton.setOnClickListener(new View.OnClickListener() {
@@ -92,7 +106,9 @@ public class MainActivity extends AppCompatActivity  {
         mRotateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rotate();
+                if(mSelectedImage != null) {
+                    rotate();
+                }
             }
         });
 
@@ -144,6 +160,58 @@ public class MainActivity extends AppCompatActivity  {
             }
         }
     }
+/*
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+*/
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        imageFileName="123";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.archit.ocr.fileprovider",
+                        photoFile);
+                camera_uri=photoURI;
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+
+
 
 
 
@@ -252,7 +320,8 @@ public class MainActivity extends AppCompatActivity  {
                                 choosePhotoFromGallary();
                                 break;
                             case 1:
-                                takePhotoFromCamera();
+                                //takePhotoFromCamera();
+                                dispatchTakePictureIntent();
                                 break;
                         }
                     }
@@ -267,12 +336,12 @@ public class MainActivity extends AppCompatActivity  {
 
         startActivityForResult(galleryIntent, GALLERY);
     }
-
+/*
     private void takePhotoFromCamera() {
         Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         startActivityForResult(intent, CAMERA);
     }
-
+*/
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
@@ -302,7 +371,24 @@ public class MainActivity extends AppCompatActivity  {
             mSelectedImage=thumbnail;
            // saveImage(thumbnail);
             Toast.makeText(MainActivity.this, "Image Saved!", Toast.LENGTH_SHORT).show();
+        } else if(requestCode==REQUEST_TAKE_PHOTO){
+            Uri contentURI = camera_uri;
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                //String path = saveImage(bitmap);
+                mSelectedImage=bitmap;
+                Toast.makeText(MainActivity.this, "Image Saved1!", Toast.LENGTH_SHORT).show();
+                mImageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Failed1!", Toast.LENGTH_SHORT).show();
+            }
+
         }
+
+
+
 
         if (mSelectedImage != null) {
             // Get the dimensions of the View
